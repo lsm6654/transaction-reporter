5cb2df08cd53c8a932ed97b423a4c2a17ceea237
sumyung-lee <sumyung-lee@navercorp.com>
2019-06-20 01:54:23 +0900

    Delete unnecessary code

--- profiler/src/main/java/com/kb/jess/profiler/repository/impl/CustomerInMemoryRepository.java
+++ profiler/src/main/java/com/kb/jess/profiler/repository/impl/CustomerInMemoryRepository.java
@@ -27,41 +27,4 @@ public class CustomerInMemoryRepository extends AbstractInMemoryRepository imple
     public Long saveCustomerNumber(Long customerNumber, String accountNumber) {
         return saveAccountNumber(accountNumber, customerNumber);
     }
-
-//    @Override
-//    public Optional<Customer> saveSessionLog(SessionStartLog sessionStartLog) {
-//        if (sessionStartLog.getCustomerNumber() < 0) {
-//            return Optional.empty();
-//        }
-//
-//        Customer customer = findCustomerByCustomerId(sessionStartLog.getCustomerNumber());
-//        customer.incrementSessionCount();
-//
-//        return Optional.of(customer);
-//    }
-//
-//    @Override
-//    public Optional<Customer> saveRegisterLog(RegisterLog registerLog) {
-//        Customer customer = new Customer(null, registerLog.getCustomerName(), registerLog.getBirthday(), registerLog.getTransactionTime(), 1);
-//        saveCustomer(registerLog.getCustomerNumber(), customer);
-//
-//        return Optional.of(customer);
-//    }
-//
-//    @Override
-//    public Optional<Customer> saveCreateAccountLog(CreateAccountLog createAccountLog) {
-//        Customer customer = findCustomerByCustomerId(createAccountLog.getCustomerNumber());
-//
-//        Account account = new Account(createAccountLog.getAccountNumber(), 0l, new ArrayList<>(), null, null);
-//        Customer newCustomer = customer.copy(account);
-//        saveCustomer(createAccountLog.getCustomerNumber(), newCustomer);
-//        saveAccountNumber(createAccountLog.getAccountNumber(), createAccountLog.getCustomerNumber());
-//
-//        return Optional.of(newCustomer);
-//    }
-//
-//    @Override
-//    public Optional<Customer> findOneByCustomerNumber(Long customerNumber) {
-//        return findCustomer(customerNumber);
-//    }
 }
